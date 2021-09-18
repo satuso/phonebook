1f5b03976ce2d42284f2537ed952e76563b7e610
satuso <73895314+satuso@users.noreply.github.com>
2021-09-18 16:04:41 +0300

    3.3

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
