3abea76e4e84451ed14eb3b1d9a2687c9b3d5e2b
satuso <73895314+satuso@users.noreply.github.com>
2021-09-19 19:22:25 +0300

    3.9

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
 const app = express()
+
+const cors = require('cors')
+app.use(cors())
+
 app.use(express.json())
 
 const morgan = require('morgan')
@@ -89,7 +92,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
